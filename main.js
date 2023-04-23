58950dfc26ee6a4c3086e7f8999c475203ebf598
kodego-mae <funj451@gmail.com>
2023-04-24 03:55:23 +0700

    final project

--- main.js
+++ main.js
@@ -3,15 +3,12 @@ import "./style.css";
 const quoteText = document.querySelector(".quote"),
   quoteBtn = document.querySelector("button"),
   authorName = document.querySelector(".name"),
-  speechBtn = document.querySelector(".speech"),
   copyBtn = document.querySelector(".copy"),
-  twitterBtn = document.querySelector(".twitter"),
-  synth = speechSynthesis;
+  twitterBtn = document.querySelector(".twitter");
 
 function randomQuote() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading Quote...";
-
   fetch("http://api.quotable.io/random")
     .then((response) => response.json())
     .then((result) => {
