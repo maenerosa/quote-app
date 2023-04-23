87758e61bece78af3b6f539bfcd39446cbc0fcd6
kodego-mae <funj451@gmail.com>
2023-04-24 00:06:34 +0700

    update js file

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const title = document.getElementById("container");
+const quote = document.getElementById("quote-text");
+const author = document.getElementById("author");
+const btnGenerate = document.getElementById("btn-generate");
+
+fetch(`https://api.quotable.io/quotes/random`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.content, data.author);
+  });
--- src/fetchQuote.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchQuote(content) {
-  const RANDOM_QUOTE = `https://api.quotable.io/quotes/random`;
-
-  const response = await fetch(RANDOM_QUOTE);
-  const fetchQuote = await response.json();
-
-  return fetchQuote;
-}
