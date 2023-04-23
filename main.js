911308606a856508b0884189e65b649520b665cb
kodego-mae <funj451@gmail.com>
2023-04-24 04:40:17 +0700

    finalize js file

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import "./style.css";
 
-const quoteText = document.querySelector(".quote"),
+const quoteContainer = document.querySelector(".quote"),
   quoteBtn = document.querySelector("button"),
   authorName = document.querySelector(".name");
 
 function randomQuote() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading Quote...";
+
   fetch("http://api.quotable.io/random")
     .then((response) => response.json())
-    .then((result) => {
-      quoteText.innerText = result.content;
-      authorName.innerText = result.author;
+    .then((quote) => {
+      quoteContainer.innerText = quote.content;
+      authorName.innerText = quote.author;
       quoteBtn.classList.remove("loading");
       quoteBtn.innerText = "New Quote";
     });
