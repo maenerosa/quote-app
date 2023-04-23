4a3483db3db0f452332da9ce381a98a989664961
kodego-mae <funj451@gmail.com>
2023-04-23 18:46:38 +0700

    update js file

---
+++ src/fetchQuote.js
@@ -0,0 +1,8 @@
+export default async function fetchQuote(content) {
+  const RANDOM_QUOTE = `https://api.quotable.io/quotes/random`;
+
+  const response = await fetch(RANDOM_QUOTE);
+  const fetchQuote = await response.json();
+
+  return weatherData;
+}
