26ee3542ab6be1b796b180d68f8a6b0d824803ff
kodego-mae <funj451@gmail.com>
2023-04-24 03:36:55 +0700

    update all file

--- src/fetchQuote.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchAllQuote(content) {
-  const randomQuote = `https://api.quotable.io/quotes/random`;
-
-  const response = await fetch(randomQuote);
-  const quoteData = await response.json();
-
-  return quoteData;
-}
