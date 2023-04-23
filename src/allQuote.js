c8a26de80dc79749c72930a30f00fb5d4aa21aa8
kodego-mae <funj451@gmail.com>
2023-04-23 18:35:28 +0700

    initial commit

--- src/allQuote.js
+++ src/allQuote.js
@@ -1,42 +1,10 @@
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 
-title.innerHTML = quote;
-author.innerHTML = "~ " + auth;
+export default function getQuote(quoteData) {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+}
 
-const text = document.getElementById("quote");
-const author = document.getElementById("author");
-const tweetButton = document.getElementById("tweet");
-
-const getNewQuote = async () => {
-  //api for quotes
-  var url = "https://api.quotable.io/quotes/random";
-
-  // fetch the data from api
-  const response = await fetch(url);
-  console.log(typeof response);
-  //convert response to json and store it in quotes array
-  const allQuotes = await response.json();
-
-  // Generates a random number between 0 and the length of the quotes array
-  const indx = Math.floor(Math.random() * allQuotes.length);
-
-  //Store the quote present at the randomly generated index
-  const quote = allQuotes[indx].text;
-
-  //Store the author of the respective quote
-  const auth = allQuotes[indx].author;
-
-  if (auth == null) {
-    author = "Anonymous";
-  }
-
-  //function to dynamically display the quote and the author
-  text.innerHTML = quote;
-  author.innerHTML = "~ " + auth;
-
-  //tweet the quote
-  tweetButton.href =
-    "https://twitter.com/intent/tweet?text=" + quote + " ~ " + auth;
-};
-getNewQuote();
+const h1 = document.createElement("h1");
+h1.textContent = `${quoteData.content}, ${getQuote.quoteData[0].author}.`;
