d1b9974e63e2b6acb5d22bfc73898ba83e6f1939
kodego-mae <funj451@gmail.com>
2023-04-24 00:31:59 +0700

    update js file

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ const quote = document.getElementById("quote-text");
 const author = document.getElementById("author");
 const btnGenerate = document.getElementById("btn-generate");
 
-function randomQuote{
-fetch(`https://api.quotable.io/quotes/random`)
-  .then((response) => response.json())
-  .then((data) => {
-    title.textContent = data.content;
-  author.textContent = data.author;
-  });
- }
+function randomQuote() {
+  fetch(`https://api.quotable.io/quotes/random`)
+    .then((response) => response.json())
+    .then((data) => {
+      title.textContent = data.content;
+      author.textContent = `-- $(data.author}`;
+    });
+}
+
+randomQuote();
