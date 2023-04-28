01c24e572db1587fd2b07f50f3d33ab9e520c5b0
Mae Nerosa <126475904+maenerosa@users.noreply.github.com>
2023-04-29 05:29:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const quoteText = document.querySelector("quote"),
+  quoteBtn = document.querySelector("btn-generator"),
+  authorName = document.querySelector("name");
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("http://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+    });
+}
+quoteBtn.addEventListener("click", randomQuote);
